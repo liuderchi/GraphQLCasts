26a6e599e1fca068eeecedb0c0aebb6998ad1a6d
liuderchi <liuderchi@gmail.com>
2017-08-14 15:36:35 +0800

    lec 10: add RootQuery but not implement resolve() yet

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,9 +1,17 @@
 const graphql = require('graphql')
+const _ = require('lodash')
 const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
+  GraphQLSchema,
 } = graphql
+
+// fake data
+const users = [
+  { id: '23', firstName: 'Bill', age: 20 },
+  { id: '47', firstName: 'Samantha', age: 21 },
+]
 
 // custom type for user
 const UserType = new GraphQLObjectType({
@@ -13,4 +21,22 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
   },
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      // NOTE provide id as arg, you can find an user
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id })
+      },
+    },
+  },
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 })
--- users/server.js
+++ users/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const expressGraphQl = require('express-graphql')
 
+const schema = require('./schema/schema')
+
 const PORT = 4000
 const app = express()
 
 app.use('/graphql', expressGraphQl({
+  schema,
   graphiql: true,
 }))
 
