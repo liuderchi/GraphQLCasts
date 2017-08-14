6a552fdc045b880b5d73f38cb54119dbfeab541d
liuderchi <liuderchi@gmail.com>
2017-08-14 15:34:45 +0800

    lec 7: add /graphql to use expressGraphQl, end up `missing schema` error message

--- users/server.js
+++ users/server.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const expressGraphQl = require('express-graphql')
 
 const PORT = 4000
 const app = express()
+
+app.use('/graphql', expressGraphQl({
+  graphiql: true,
+}))
 
 app.listen(PORT, () => {
   console.log(`listening ${PORT}`)
