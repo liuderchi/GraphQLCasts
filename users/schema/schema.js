38d08c0924098b9c60472604152ad467e7d3f65a
liuderchi <liuderchi@gmail.com>
2017-08-14 20:14:32 +0800

    lec 16: add companyId to users, add companies data in db.json
    - json-server can give list of users working to some company

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -5,17 +5,41 @@ const {
   GraphQLString,
   GraphQLInt,
   GraphQLSchema,
+  GraphQLList,
 } = graphql
 
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then(resp => resp.data)
+      },
+    },
+  }),
+})
 
 // custom type for user
 const UserType = new GraphQLObjectType({
   name: 'User',   // type name
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
-  },
+    company: {
+      type: CompanyType,
+      resolve(parentValue) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(resp => resp.data)
+      },
+    },
+  }),
 })
 
 const RootQuery = new GraphQLObjectType({
@@ -28,6 +52,14 @@ const RootQuery = new GraphQLObjectType({
       resolve(parentValue, args) {
         return axios.get(`http://localhost:3000/users/${args.id}`)
           .then(resp => resp.data)  // NOTE axios put data in resp.data
+      },
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${args.id}`)
+          .then(resp => resp.data)
       },
     },
   },
